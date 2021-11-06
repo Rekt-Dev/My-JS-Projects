0820b987fd35ccdb1eaf86b26eabf1b579e0086a
rektDev <rektdevvv@gmail.com>
2021-11-06 12:36:48 +0200

    added many files to commit, testing testing 123.

---
+++ barZik.js
@@ -0,0 +1,26 @@
+function (arg1, arg2){
+	let sum = 0 ;
+	let error = "These r not numbers sire/maam"
+
+	if (typeof arg1 && typeof arg2 === "number"){
+		
+			sum = arg1 + arg2
+				
+					return sum;
+		
+		
+	
+	}
+	
+	else 
+		
+		{
+			
+				throw error
+		}
+	
+}
+
+
+
+;
