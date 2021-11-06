f9c936cdcbafc5f2b6869bfabd06941230253845
rektDev <rektdevvv@gmail.com>
2021-11-06 18:46:35 +0200

    codewars commits

---
+++ code wars/likes.js
@@ -0,0 +1,59 @@
+function likes(names)
+
+{
+
+
+
+    let likeString
+
+    for (i = 0; i < names.length; i++) {
+
+        if (names === '') {
+            likeString = 'no one likes this'
+        }
+
+        if (names.length === 1) {
+
+            likeString = `${names} ${names[0]} likes this`
+        }
+
+        if (names.length === 2) {
+
+            likeString = `${names}-->${names[0]} and ${names[1]} like this`
+        }
+
+        if (names.length === 3) {
+
+            likeString = `${names}-->${names[0]}, ${names[1]} and ${names[2]} like this`
+
+
+
+        }
+
+        if (names.length === 4) {
+
+            likeString = `${names}-->${names[0]},${names[1]} and two others like this`
+
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+    let param = likeString
+
+    console.log(names.length)
+
+    return param
+
+}
+
+var result = likes([''])
+console.log(result)
