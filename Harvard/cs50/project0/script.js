c2c5bfbd61c0cf643a2fd970bfd9484209465ef2
rektDev <rektdevvv@gmail.com>
2021-11-19 06:38:29 +0200

    lolol

--- Harvard/cs50/project0/script.js
+++ Harvard/cs50/project0/script.js
@@ -5,7 +5,7 @@ const classNames = {
   TODO_DELETE: 'todo-delete',
 }
 
-let list = document.getElementById('todolistlocation')
+let list = document.getElementById('todo-list')
 let itemCountSpan = document.getElementById('item-count')
 let uncheckedCountSpan = document.getElementById('unchecked-count')
 let para = document.getElementById('para')
@@ -20,10 +20,12 @@ let button = document.getElementById('button center')
 let toDoCounter=1
 let uncheckedCounter=1
 let array=[]
+
+
+
 function newTodo() {
   addElement()
 array.push(addElement)
-console.log(`this is the to do list in array format --> ${array}`)
   itemCountSpan.innerHTML=toDoCounter
   uncheckedCounter = toDoCounter
 
@@ -34,6 +36,8 @@ console.log(`this is the to do list in array format --> ${array}`)
   uncheckedCounter = toDoCounter
   console.log(`this is todocounter${toDoCounter}`)
   console.log(`this is uncheckedcounter${uncheckedCounter}`)
+  console.log(`this is the to do list in array format --> ${array}`)
+
 
   return toDoCounter
  
@@ -42,6 +46,8 @@ console.log(`this is the to do list in array format --> ${array}`)
 
 
 let toDoCounterResult= newTodo()
+let geekFunc = geek()
+console.log(geekFunc)
 //button.onclick=
 //let userInput = window. prompt("Please enter your todo item: "); 
 function addElement () {
@@ -52,7 +58,8 @@ function addElement () {
   let newContent = document.createTextNode(userInput);
   // add the text node to the newly created div
   newLi.appendChild(newContent);
-  let ulId = document.getElementById("todo-list") 
+  
+ // let ulId = document.getElementById("ulId") 
 
   // add the newly created element and its content into the DOM
   let currentDiv = document.getElementById("ulId");
@@ -60,8 +67,36 @@ function addElement () {
 
 return userInput
 }
-/*
 
 
+  function geek() {
+            var myDiv = document.getElementById("myDiv");
+             
+            // creating checkbox element
+            var checkbox = document.createElement('input');
+             
+            // Assigning the attributes
+            // to created checkbox
+            checkbox.type = "checkbox";
+            checkbox.name = "name";
+            checkbox.value = "value";
+            checkbox.id = "id";
+             
+            // creating label for checkbox
+            var label = document.createElement('label');
+             
+            // assigning attributes for
+            // the created label tag
+            label.htmlFor = "id";
+             
+            // appending the created text to
+            // the created label tag
+            label.appendChild(document.createTextNode('This is the label for checkbox.'));
+             
+            // appending the checkbox
+            // and label to div
+            myDiv.appendChild(checkbox);
+            myDiv.appendChild(label);
+        }
 
-*/
+geek()
