0ea5d1d223dc6321561e41f2bc5fa4f2b1fcaeb9
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-02-15 18:42:23 +0200

    Update nodeServer.js

--- NodeServer/nodeServer.js
+++ NodeServer/nodeServer.js
@@ -1,11 +1,11 @@
 import { createServer } from 'http';
 import { readFileSync } from 'fs';
 
-//const data = require()
-const server = createServer((req, res) => {
+//const data=require()
+const server=createServer((req, res) => {
 
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead (200, { 'Content-Type': 'text/html' });
     let html = readFileSync('./index.html');
     res.end(html) ;
 
